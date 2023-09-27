ef492f31159e2b1fdc3e8c06e42113d8f817d6fc
QuanNguyen208 <quan.nguyen17@hcmut.edu.vn>
2023-09-27 15:56:56 +0700

    ex1

---
+++ Core/Inc/7seg_clock.h
@@ -0,0 +1,14 @@
+/*
+ * 7seg_clock.h
+ *
+ *  Created on: Sep 27, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_7SEG_CLOCK_H_
+#define INC_7SEG_CLOCK_H_
+
+void display7SEG(int index_led) ;
+void update7SEG(int index_led, int val) ;
+
+#endif /* INC_7SEG_CLOCK_H_ */
---
+++ Core/Src/7seg_clock.c
@@ -0,0 +1,146 @@
+/*
+ * 7seg_clock.c
+ *
+ *  Created on: Sep 27, 2023
+ *      Author: Admin
+ */
+
+#include "main.h"
+#include "7seg_clock.h"
+
+#define MAX_LED 4
+int led_buffer[MAX_LED] ;
+
+void update7SEG(int index_led , int val){
+		// save value for clk
+
+		if (index_led < MAX_LED && val < 10);
+		led_buffer[index_led] = val;
+
+
+}
+void display7SEG(int index_led){
+	int temp = led_buffer[index_led] ;
+
+	// choose clock
+			switch (index_led){
+			case 0:
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+
+				break;
+
+			case 1:
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+
+				break;
+
+
+
+			default:
+				break;
+			}
+
+	switch(temp){
+				case 0:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_SET ) ;
+					break;
+				case 1:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_SET ) ;
+					break;
+				case 2:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				case 3:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				case 4:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_SET ) ;
+					break;
+				case 5:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				case 6:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				case 7:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_SET ) ;
+					break;
+				case 8:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				case 9:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+					break;
+				default:
+					HAL_GPIO_WritePin ( LED7_A_GPIO_Port , LED7_A_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_B_GPIO_Port , LED7_B_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_C_GPIO_Port , LED7_C_Pin , GPIO_PIN_SET ) ;
+					HAL_GPIO_WritePin ( LED7_D_GPIO_Port , LED7_D_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_E_GPIO_Port , LED7_E_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_F_GPIO_Port , LED7_F_Pin , GPIO_PIN_RESET ) ;
+					HAL_GPIO_WritePin ( LED7_G_GPIO_Port , LED7_G_Pin , GPIO_PIN_RESET ) ;
+
+				}
+}
